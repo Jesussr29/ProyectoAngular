3aec3864a2ed77cd5fdea788bb61bac4f3e17dbb
jesussr29 <jesus.rincon-albarran@iesruizgijon.com>
2025-03-10 02:09:32 +0100

    modificacion del app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ListadoClienteComponent } from './listado-cliente/listado-cliente.component';
-import { ListadoProductosComponent } from './listado-productos/listado-productos.component';
-import { Error404Component } from './error404/error404.component';
-import { provideHttpClient } from '@angular/common/http';
-// import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    ListadoClienteComponent,
-    ListadoProductosComponent,
-    Error404Component
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // HttpClientModule
+    HttpClientModule
   ],
-  providers: [
-    provideHttpClient()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
