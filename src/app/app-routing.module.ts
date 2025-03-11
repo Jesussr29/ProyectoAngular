9e86e9d7111285e4ed24fff2ff9ea70f0873ba59
jesussr29 <jesus.rincon-albarran@iesruizgijon.com>
2025-03-11 16:45:38 +0100

    he modificado el componente de error 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { CartasComponent } from './components/cartas/cartas-component.component';
 import { HomeComponent } from './components/home/home.component';
 import { AcercaComponent } from './components/acercaDe/acerca-component.component';
+import { Error404Component } from './components/error404/error404.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'acerca', component: AcercaComponent },
   { path: 'cartas', component: CartasComponent },
-  { path: '**', redirectTo: '/home' } // Si la ruta no existe, redirige a Home
+  { path: 'error', component: Error404Component },
+  { path: '**', redirectTo: '/error' } // Si la ruta no existe, redirige a Home
 ];
 
 @NgModule({
